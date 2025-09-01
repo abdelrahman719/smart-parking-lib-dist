87473272d2bb244e7b4567b98a34088f10c47982
AbdelrahmanSamir <55239369+abdelrahman719@users.noreply.github.com>
2025-09-01 13:35:48 +0300

    updates for search input

--- lib/components/custom-reactive-search-input/custom-reactive-search-input.component.d.ts
+++ lib/components/custom-reactive-search-input/custom-reactive-search-input.component.d.ts
@@ -4,6 +4,7 @@ export declare class CustomReactiveSearchInputComponent {
     model: string;
     modelChange: EventEmitter<string>;
     search: EventEmitter<string>;
+    headerSearchIcon: import("@angular/core").InputSignal<boolean>;
     containerClass: string;
     inputClass: string;
     inputPlaceholder: string;
@@ -11,5 +12,5 @@ export declare class CustomReactiveSearchInputComponent {
     constructor();
     onInputChange(value: string): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomReactiveSearchInputComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CustomReactiveSearchInputComponent, "custom-reactive-search-input", never, { "model": { "alias": "model"; "required": false; }; "containerClass": { "alias": "containerClass"; "required": true; }; "inputClass": { "alias": "inputClass"; "required": true; }; "inputPlaceholder": { "alias": "inputPlaceholder"; "required": true; }; }, { "modelChange": "modelChange"; "search": "search"; }, never, never, true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomReactiveSearchInputComponent, "custom-reactive-search-input", never, { "model": { "alias": "model"; "required": false; }; "headerSearchIcon": { "alias": "headerSearchIcon"; "required": false; "isSignal": true; }; "containerClass": { "alias": "containerClass"; "required": true; }; "inputClass": { "alias": "inputClass"; "required": true; }; "inputPlaceholder": { "alias": "inputPlaceholder"; "required": true; }; }, { "modelChange": "modelChange"; "search": "search"; }, never, never, true, never>;
 }

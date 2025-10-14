ec90ad58e5a23c057ba0a9fd444123a2b89c843b
AbdelrahmanSamir <55239369+abdelrahman719@users.noreply.github.com>
2025-10-14 09:55:50 +0300

    update action

--- lib/components/custom-actions-dropdown/custom-actions-dropdown.component.d.ts
+++ lib/components/custom-actions-dropdown/custom-actions-dropdown.component.d.ts
@@ -1,3 +1,4 @@
+import { ElementRef, Renderer2 } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import * as i0 from "@angular/core";
 export interface IActions {
@@ -7,20 +8,31 @@ export interface IActions {
 }
 export declare class CustomActionsDropdownComponent {
     private sanitizer;
+    private renderer;
+    private elRef;
     actions: IActions[];
     context: any;
     horizontalDots: boolean;
     hasActionTemplate: boolean;
     expandSide: 'RIGHT' | 'LEFT';
     expandDirection: 'TOP' | 'BOTTOM';
-    isOpen: boolean;
-    constructor(sanitizer: DomSanitizer);
+    actionsPopup?: ElementRef<HTMLDivElement>;
+    triggerWrapper?: ElementRef<HTMLDivElement>;
+    actionDropdownContainer?: ElementRef<HTMLDivElement>;
+    private isAppendedToBody;
+    isOpen: import("@angular/core").WritableSignal<boolean>;
+    showAnimation: import("@angular/core").WritableSignal<boolean>;
+    constructor(sanitizer: DomSanitizer, renderer: Renderer2, elRef: ElementRef);
     sanitizeSvg(svg: string): SafeHtml;
     openDropdown(): void;
     closeDropdown(): void;
+    private portalToBody;
+    private updatePosition;
+    private detachFromBody;
     onClickAction(action: {
         callback: (ctx: any) => void;
     }, event: MouseEvent): void;
+    ngOnDestroy(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomActionsDropdownComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<CustomActionsDropdownComponent, "custom-actions-dropdown", never, { "actions": { "alias": "actions"; "required": false; }; "context": { "alias": "context"; "required": false; }; "horizontalDots": { "alias": "horizontalDots"; "required": false; }; "hasActionTemplate": { "alias": "hasActionTemplate"; "required": false; }; "expandSide": { "alias": "expandSide"; "required": false; }; "expandDirection": { "alias": "expandDirection"; "required": false; }; }, {}, never, ["*"], true, never>;
 }

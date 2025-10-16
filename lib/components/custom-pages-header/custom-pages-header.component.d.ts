69bab64cda0f3c10fbb43cf7cbee9ac84e5f33c5
AbdelrahmanSamir <55239369+abdelrahman719@users.noreply.github.com>
2025-10-16 07:47:52 +0300

    save

--- lib/components/custom-pages-header/custom-pages-header.component.d.ts
+++ lib/components/custom-pages-header/custom-pages-header.component.d.ts
@@ -5,6 +5,7 @@ export declare class CustomPagesHeaderComponent {
     headerTitle: import("@angular/core").InputSignal<string>;
     btnTitle: import("@angular/core").InputSignal<string>;
     hasTabs: import("@angular/core").InputSignal<boolean>;
+    hideBtn: import("@angular/core").InputSignal<boolean>;
     listCounter: import("@angular/core").InputSignal<number>;
     pageTabs: import("@angular/core").InputSignal<ITabDropdownOption[]>;
     selectedTab: import("@angular/core").WritableSignal<ITabDropdownOption>;
@@ -14,5 +15,5 @@ export declare class CustomPagesHeaderComponent {
     selectTab(tab: ITabDropdownOption): void;
     onAddClick(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomPagesHeaderComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CustomPagesHeaderComponent, "custom-pages-header", never, { "headerTitle": { "alias": "headerTitle"; "required": false; "isSignal": true; }; "btnTitle": { "alias": "btnTitle"; "required": false; "isSignal": true; }; "hasTabs": { "alias": "hasTabs"; "required": false; "isSignal": true; }; "listCounter": { "alias": "listCounter"; "required": false; "isSignal": true; }; "pageTabs": { "alias": "pageTabs"; "required": false; "isSignal": true; }; }, { "tabSelected": "tabSelected"; "addAction": "addAction"; }, never, never, true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomPagesHeaderComponent, "custom-pages-header", never, { "headerTitle": { "alias": "headerTitle"; "required": false; "isSignal": true; }; "btnTitle": { "alias": "btnTitle"; "required": false; "isSignal": true; }; "hasTabs": { "alias": "hasTabs"; "required": false; "isSignal": true; }; "hideBtn": { "alias": "hideBtn"; "required": false; "isSignal": true; }; "listCounter": { "alias": "listCounter"; "required": false; "isSignal": true; }; "pageTabs": { "alias": "pageTabs"; "required": false; "isSignal": true; }; }, { "tabSelected": "tabSelected"; "addAction": "addAction"; }, never, never, true, never>;
 }

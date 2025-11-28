fe37b23586d58d679bd521daecdf3e0bbfdb2bc1
AbdelrahmanSamir <55239369+abdelrahman719@users.noreply.github.com>
2025-11-28 02:33:30 +0200

    deploy

--- lib/smart-parking-shared-lib.service.d.ts
+++
@@ -1,6 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class SmartParkingSharedLibService {
-    constructor();
-    static ɵfac: i0.ɵɵFactoryDeclaration<SmartParkingSharedLibService, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<SmartParkingSharedLibService>;
-}

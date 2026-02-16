397bd4429a43f038652b26e0abf1cc4e0a8885e6
AbdelrahmanSamir <55239369+abdelrahman719@users.noreply.github.com>
2026-02-16 03:35:49 +0200

    hot fix

--- fesm2022/smart-parking-shared-lib.mjs
+++ fesm2022/smart-parking-shared-lib.mjs
@@ -7932,10 +7932,6 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.2.17", ngImpo
         }], ctorParameters: () => [] });
 
 function checkProductAccess(product) {
-    let tenantPlatformService = inject(TenantPlatformService);
-    if (!tenantPlatformService.isTenantPlatform()) {
-        return true;
-    }
     const userData = localStorage.getItem('userData');
     if (!userData) {
         window.location.hash = 'tenant-auth';

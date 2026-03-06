7d4d8340292d98cdc1f95c1164abd59d89de7b15
AbdelrahmanSamir <55239369+abdelrahman719@users.noreply.github.com>
2026-03-06 15:28:17 +0200

    deploy

--- lib/components/custom-reactive-search-input/custom-reactive-search-input.component.d.ts
+++ lib/components/custom-reactive-search-input/custom-reactive-search-input.component.d.ts
@@ -4,6 +4,7 @@ export declare class CustomReactiveSearchInputComponent {
     model: string;
     modelChange: EventEmitter<string>;
     search: EventEmitter<string>;
+    clear: EventEmitter<void>;
     headerSearchIcon: import("@angular/core").InputSignal<boolean>;
     containerClass: string;
     inputClass: string;
@@ -16,5 +17,5 @@ export declare class CustomReactiveSearchInputComponent {
     preventInvalidNumberInput(event: KeyboardEvent): void;
     preventPaste(event: ClipboardEvent): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CustomReactiveSearchInputComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CustomReactiveSearchInputComponent, "custom-reactive-search-input", never, { "model": { "alias": "model"; "required": false; }; "headerSearchIcon": { "alias": "headerSearchIcon"; "required": false; "isSignal": true; }; "containerClass": { "alias": "containerClass"; "required": true; }; "inputClass": { "alias": "inputClass"; "required": true; }; "inputPlaceholder": { "alias": "inputPlaceholder"; "required": true; }; "validateNumber": { "alias": "validateNumber"; "required": false; }; }, { "modelChange": "modelChange"; "search": "search"; }, never, never, true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CustomReactiveSearchInputComponent, "custom-reactive-search-input", never, { "model": { "alias": "model"; "required": false; }; "headerSearchIcon": { "alias": "headerSearchIcon"; "required": false; "isSignal": true; }; "containerClass": { "alias": "containerClass"; "required": true; }; "inputClass": { "alias": "inputClass"; "required": true; }; "inputPlaceholder": { "alias": "inputPlaceholder"; "required": true; }; "validateNumber": { "alias": "validateNumber"; "required": false; }; }, { "modelChange": "modelChange"; "search": "search"; "clear": "clear"; }, never, never, true, never>;
 }
